7037b20deab8c35e69c213e39f09459122daefed
Luan Oliveira <luanvisouza037@gmail.com>
2025-08-30 15:23:57 -0300

    Add method in CarService.cs and, question in Program.cs

--- DesafioCadastro/DesafioCadastro/Program.cs
+++ DesafioCadastro/DesafioCadastro/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace DesafioCadastro;
+﻿using DesafioCadastro.Entities;
+
+namespace DesafioCadastro;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        List<Car> cars = new List<Car>();
         
+        Console.Write("Quantos carros voce quer cadastrar? ");
+        int n = int.Parse(Console.ReadLine()!);
+
+
+        for (int i = 0; i < n; i++)
+        {
+            
+        }
     }
 }
---
+++ DesafioCadastro/DesafioCadastro/Service/CarService.cs
@@ -0,0 +1,21 @@
+﻿using DesafioCadastro.Entities;
+
+namespace DesafioCadastro.Service;
+
+public class CarService
+{
+    public void FindCheapest(List<Car> cars)
+    {
+        Car max = cars[0];
+
+        foreach (Car car in cars)
+        {
+            if (car.Price < max.Price)
+            {
+                max = car;
+            }
+        }
+        
+        Console.WriteLine(max);
+    }
+}
