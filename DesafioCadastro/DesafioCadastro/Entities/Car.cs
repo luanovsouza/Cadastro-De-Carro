2672e23840f917b356110d36326b00abc3eb7470
Luan Oliveira <luanvisouza037@gmail.com>
2025-08-30 15:13:30 -0300

    Add Car class, with name and Price

---
+++ DesafioCadastro/DesafioCadastro/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace DesafioCadastro.Entities;
+
+public class Car
+{
+    public string? Name { get; set; }
+    public double Price { get; set; }
+
+
+    public Car(string? name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"Car Name: {Name}, Price $ {Price.ToString("F2")}";
+    }
+}
